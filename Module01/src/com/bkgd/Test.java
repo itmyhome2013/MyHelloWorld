f7f76f9bab517c6253cc69d854f10e147da23f57
itmyhome2013 <itmyhome@163.com>
2020-07-14 18:37:47 +0800

    Initial commit

---
+++ Module01/src/com/bkgd/Test.java
@@ -0,0 +1,37 @@
+package com.bkgd;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+    public static final int i = 1;
+    /**
+     * hello 你好
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        System.out.println("xxxx");
+        ArrayList<String> list = new ArrayList<String>();
+        Map map = new HashMap();
+        foo();
+        System.out.println();
+        System.out.println("args = " + Arrays.deepToString(args));
+
+        for (int i = 0; i < ; i++) {
+            
+        }
+
+        for (String s : list) {
+            
+        }
+
+      }
+
+
+      public static void  foo(){
+        System.out.println("hahahaha");
+      }
+}
